2c3eac9af237da433808323c170ba7672ace6073
maniak maniak <maniak88@ukr.net>
2021-12-30 13:57:43 +0200

    maniak is here upd.3

--- file1.js
+++ file1.js
@@ -2,5 +2,15 @@ const car = {type:"Ford", model:"GT500", color:"white"};
 
 function myFunction1() {
     let carName = "Volvo";
-    return 1;
+    return 525;
+  }
+
+  function myFunction2() {
+    let carName = "Volvo";
+    return 2;
+  }
+
+  function myFunction3() {
+    let carName = "Volvo";
+    return 4;
   }
